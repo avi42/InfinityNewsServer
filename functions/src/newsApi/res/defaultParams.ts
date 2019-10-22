20c21726503f3b6e5b4979f5619baecfd8622590
Avi <avigoel3027@gmail.com>
2019-10-21 19:01:49 -0700

    removed usa today from sources

--- functions/src/newsApi/res/defaultParams.ts
+++ functions/src/newsApi/res/defaultParams.ts
@@ -5,7 +5,7 @@ export const defaultParams = {
     language: "en",
     sortBy: "relevancy",
     from: getLastWeekDateStr(),
-    sources: "abc-news,associated-press,axios,bbc-news,bloomberg,politico,usa-today,the-hill,the-wall-street-journal,reuters,fortune",
+    sources: "abc-news,associated-press,axios,bbc-news,bloomberg,politico,the-hill,the-wall-street-journal,reuters,fortune",
     pageSize: 20,
     apiKey: newsKey
 };
