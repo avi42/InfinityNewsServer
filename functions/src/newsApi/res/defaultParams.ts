d61dc65b5aa64323b9989791710a7219451f2404
Avi <avigoel3027@gmail.com>
2019-10-21 18:57:30 -0700

    removed cbs news from sources

--- functions/src/newsApi/res/defaultParams.ts
+++ functions/src/newsApi/res/defaultParams.ts
@@ -5,7 +5,7 @@ export const defaultParams = {
     language: "en",
     sortBy: "relevancy",
     from: getLastWeekDateStr(),
-    sources: "abc-news,associated-press,axios,bbc-news,bloomberg,cbs-news,politico,usa-today,the-hill,the-wall-street-journal,reuters,fortune",
+    sources: "abc-news,associated-press,axios,bbc-news,bloomberg,politico,usa-today,the-hill,the-wall-street-journal,reuters,fortune",
     pageSize: 20,
     apiKey: newsKey
 };
